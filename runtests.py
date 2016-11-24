44526e4b9cd14fff0c4657510f9b04ab45f1a2dc
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-11-24 19:44:27 +0100

    Try to fix travis config

--- runtests.py
+++ runtests.py
@@ -39,7 +39,7 @@ try:
 except ImportError:
     import traceback
     traceback.print_exc()
-    msg = "To fix this error, run: pip install -r requirements_test.txt"
+    msg = "To fix this error, run: pip install -r requirements/test.txt"
     raise ImportError(msg)
 
 
