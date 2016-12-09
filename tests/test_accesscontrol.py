cc45675de6404c4f73cd8c67887225df65d86d55
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-12-09 19:54:06 +0100

    Update template branch

--- tests/test_accesscontrol.py
+++ tests/test_accesscontrol.py
@@ -10,12 +10,16 @@ import accesscontrol
 
 
 class MainTestCase(TestCase):
-    """Main Django test case"""
+    """Main Django test case."""
+
     def setUp(self):
+        """Setup method."""
         pass
 
     def test_main(self):
+        """Main test method."""
         assert accesscontrol
 
     def tearDown(self):
+        """Tear down method."""
         pass
